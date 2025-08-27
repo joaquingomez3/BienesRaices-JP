127019f8817ccce4b6f7db04df6044a2b196579a
Pablo Damián Alcaraz <pablodalcaraz@gmail.com>
2025-08-26 21:22:18 -0300

    vistas usuario

--- Repositorios/RepositorioBase.cs
+++ Repositorios/RepositorioBase.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +12,11 @@ public abstract class RepositorioBase
 		protected readonly IConfiguration configuration;
 		protected readonly string connectionString;
 
-		protected RepositorioBase(IConfiguration configuration)
-		{
-			this.configuration = configuration;
-			connectionString = configuration["ConnectionString:MySql"];
+	protected RepositorioBase(IConfiguration configuration)
+	{
+		this.configuration = configuration;
+		connectionString = configuration["ConnectionString:MySql"] ??
+			throw new InvalidOperationException("Falta la cadena de conexión MySql en la configuración.");
 		}
 	}
 
